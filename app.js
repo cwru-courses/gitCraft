106671a6cff76040be25e48d77b5c231bff073e6
aziets399 <axz399@case.edu>
2024-07-24 20:49:08 -0400

    node setup

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const swaggerUI = require("swagger-ui-express");
+
+const path = require("path");
+require("dotenv").config();
+var cors = require("cors");
+
+const swaggerSpec = require("./utilities/swagger");
+const userRoute = require("./routes/user-route");
+const { verifyJwtToken } = require("./utilities/jwt-auth");
+const { mongodbConnection } = require("./utilities/mongo-config");
+
+const port = process.env.PORT || 8080;
+
+mongodbConnection();
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+try {
+  app.use("/user", userRoute);
+} catch (error) {
+  res.status(400).json({
+    message: error.message,
+    error: "Error while performing user operations",
+  });
+}
+
+// Serve Swagger documentation
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+
+app.get(express.static(path.join(__dirname, "../client/build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../client/build/index.html"),
+    (err) => err ?? res.status(500).send(err)
+  );
+});
+
+app.listen(port, () => {
+  console.log(`git-craft server is listening on port ${port} ...`);
+});
