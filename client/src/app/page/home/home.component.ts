eebe967f71efa41a2936eb4f204f7eaf8db0b3e6
Shanmuga <1999ganesh2@gmail.com>
2024-07-26 22:07:42 -0400

    landing page initial commit

--- client/src/app/page/home/home.component.ts
+++ client/src/app/page/home/home.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatStepperModule,
+    MatIconModule, MatMenuModule, MatCardModule, MatButtonModule, MatTooltipModule, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  avatarUrl: string = `https://i.pravatar.cc/150?img=${Math.floor(Math.random() * 70)}`;
   constructor() {
   }
 
